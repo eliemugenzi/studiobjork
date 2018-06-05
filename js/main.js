b90b3968f45d15af8121783f0c71e8423f136ef3
eliemugenzi <eliemugenzi@gmail.com>
2018-06-05 13:47:15 +0200

    Add mobile navigation menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(()=>{
+    $('#nav-mobile').show();
     $('.transition').addClass('anim-trans');
+    $('.menu-mobile menu').hide();
+
+    $('i.fa.fa-bars,i.fa.fa-times-circle').click(()=>{
+        $('.menu-sm').slideToggle();
+    })
 })
