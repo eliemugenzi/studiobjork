50e450fa78902fa6f77f6ce5682ce50e13cd2c14
eliemugenzi <eliemugenzi@gmail.com>
2018-06-05 19:16:40 +0200

    Other changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ $(document).ready(()=>{
     $('.transition').addClass('anim-trans');
     $('.menu-mobile menu').hide();
 
-    $('i.fa.fa-bars,i.fa.fa-times-circle').click(()=>{
-        $('.menu-sm').slideToggle();
+    $('i.fa.fa-bars').click(()=>{
+        $('.menu-sm').slideDown();
+    })
+    $('i.fa.fa-times-circle').click(()=>{
+        $('.menu-sm').fadeOut();
+        console.log('clicked');
     })
 })
