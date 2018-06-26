52c3cb6b494cf69fe8381b0d01570f2a9827cf10
eliemugenzi <eliemugenzi@gmail.com>
2018-06-26 10:07:15 +0200

    Change fonts and fix navigation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(()=>{
-    $('#nav-mobile').show();
+    
     $('.transition').addClass('anim-trans');
     $('.menu-mobile menu').hide();
 
-    $('#nav-mobile button').click(()=>{
+    $('nav button').click(()=>{
         console.log("Sth happens")
         $('.menu-sm').slideDown();
     })
+    
     $('.menu-sm button').click(()=>{
         $('.menu-sm').slideUp();
         console.log('clicked');
